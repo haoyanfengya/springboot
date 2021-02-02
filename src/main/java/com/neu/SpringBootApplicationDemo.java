b5a6b2969265ee4b9464234ff9d99df7bd60f9b7
haoyanfeng <haoyanfeng@datacraftman.com>
2021-02-02 17:02:35 +0800

    change

--- src/main/java/com/neu/SpringBootApplicationDemo.java
+++ src/main/java/com/neu/SpringBootApplicationDemo.java
@@ -3,15 +3,22 @@ package com.neu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@MapperScan("com.neu.mapper")
+@MapperScan("com.neu.dao")
 @EnableCaching//启用缓存
-public class SpringBootApplicationDemo  {
-		public static void main(String[] args) {
-			SpringApplication.run(SpringBootApplicationDemo.class, args);
-		}
-		
+public class SpringBootApplicationDemo extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationDemo.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder  builder) {
+        return builder.sources(SpringBootApplicationDemo.class);
+    }
+
 }
 
